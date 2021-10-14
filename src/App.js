9c99b56e4896bb67ba48148e4a0887298ae227d5
120356aa <yurlovandrew@gmail.com>
2021-10-14 15:50:55 -0700

    routing setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-function App() {
-  return (
-    <div className="App">
+import React, { useEffect } from 'react'
+import { BrowserRouter, Roue, Switch, useHistory } from 'react-router-dom'
 
+function App() {
+  useEffect( _ => {
+    let history = useHistory();
+    
+  }, [])
+
+  return (
+    <div>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/signin">
+
+          </Route>
+          <Route path="/notes">
+
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
